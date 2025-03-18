7a110239e24e10957a01dadef1a122a24c636d4a
Migue0301 <miguel.paredesperez1@gmail.com>
2025-03-18 00:19:25 -0600

    15 Add properties from nextjs

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 
 import SearchFilters from "./SearchFilters";
 import UserNav from "./UserNav";
+import { getUserId } from "@/app/lib/actions";
 import AddPropertyButton from "./AddPropertyButton";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const userId = await getUserId();
+
+    console.log('userId:', userId);
+
     return(
         <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
             <div className="max-w-[1500px] mx-auto px-6">
@@ -24,9 +29,14 @@ const Navbar = () => {
                     </div>
 
                     <div className="flex items-center space-x-6">
-                        <AddPropertyButton/>
+                        <AddPropertyButton
+                            userId={userId}
+                        />
+                            
 
-                        <UserNav />
+                        <UserNav 
+                            userId={userId}
+                        />
                     </div>
                 </div>
             </div>
--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,11 +1,45 @@
+'use client'
+
+import apiService from "@/app/services/apiService";
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 
+export type PropertyType = {
+    id: string;
+    title: string;
+    image_url: string;
+    price_per_night: number;
+    is_favorite: boolean;
+}
+
 const PropertyList = () => {
-    return (
+
+    const [properties, setProperties] = useState<PropertyType[]>([])
+
+    const getProperties = async () => {
+
+        const tmpProperties = await apiService.get('/api/properties/')
+
+        setProperties(tmpProperties.data)
+    }
+
+    
+    useEffect(() => {
+        getProperties();
+    }, [] )
+    return(
+
+        
         <>
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
+        {properties.map((property) => {
+            return(
+                <PropertyListItem
+                    key={property.id}
+                    property={property}
+
+                />
+            )
+        })}
         </>
     )
 }
